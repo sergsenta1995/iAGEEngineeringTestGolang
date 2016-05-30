d5e4a25cc7595396106f6775db43284e83477d77
sergsenta1995 <sergsenta1995@gmail.com>
2016-05-30 21:02:19 +0400

    update test main_test.go update IntToNumber main.go

--- main.go
+++ main.go
@@ -3,21 +3,81 @@ package main
 import "fmt"
 
 var ones = []string {
-	"один",
-	"два",
-	"три",
-	"четыре",
-	"пять",
-	"шесть",
-	"семь",
-	"восемь",
-	"девять",
-}
+	"один ",
+	"два ",
+	"три ",
+	"четыре ",
+	"пять ",
+	"шесть ",
+	"семь ",
+	"восемь ",
+	"девять "}
+
+var triads = []string{
+	"",
+	"тысяча ",
+	"миллион "}
+
+var onesTens = []string{
+	"одиннадцать ",
+	"двенадцать ",
+	"тринадцать ",
+	"четырнадцать ",
+	"пятнадцать ",
+	"шестнадцать ",
+	"семнадцать ",
+	"воесемнадцать ",
+	"девятнадцать "}
+
+var tens = []string{
+	"десять ",
+	"двадцать ",
+	"тридцать ",
+	"сорок ",
+	"пятдесят ",
+	"шестдесят ",
+	"семдесят ",
+	"восемдесят ",
+	"девяносто "}
+
+var thousands = []string{
+	"сто ",
+	"двести ",
+	"триста ",
+	"четыреста ",
+	"пятсот ",
+	"шестьсот ",
+	"семсот ",
+	"восемсот ",
+	"девятсот "}
 
 func IntToNumeric(number int) string {
-	return ones[number-1]
+	var triadNumeric string
+	var result string
+	for triadNumber := 0; number >= 1; triadNumber++ {
+		triad := number % 1000
+		number /= 1000
+		if triad >= 100 {
+			triadNumeric += thousands[triad/100-1]
+			triad %= 100
+		}
+		if triad >= 20 || triad == 10 {
+			triadNumeric += tens[triad/10-1]
+			triad %= 10
+		}
+		if triad >= 11 {
+			triadNumeric += onesTens[triad/10-1]
+		}
+		if triad == 1 {
+			triadNumeric += ones[triad-1]
+			triadNumeric += triads[triadNumber]
+		}
+		result = triadNumeric + result
+		triadNumeric = ""
+	}
+	return result[0:len(result)-1]
 }
 
 func main ()  {
-	fmt.Println("!!!")
+	fmt.Println(IntToNumeric(1000))
 }
--- main_test.go
+++ main_test.go
@@ -6,23 +6,23 @@ import (
 
 type testpair struct {
 	value int
-	numeric int
+	numeric string
 }
 
 var tests = []testpair {
 	{1, "один"},
 	{10, "десять"},
 	{100, "сто"},
-	{1000, "тысяча"}}
+	{1000, "один тысяча"}}
 
 func TestIntToNumeric(t *testing.T) {
 	for _, pair := range tests {
 		result := IntToNumeric(pair.value)
 		if result != pair.numeric {
 			t.Error(
-				"Для значения ", pair.value,
-				"допустиый результат ", pair.numeric,";",
-				"Получено ", result,
+				"Для значения <", pair.value,">;",
+				"допустиый результат <", pair.numeric,">;",
+				"Получено <", result,">",
 			)
 		}
 	}
