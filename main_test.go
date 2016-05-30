88d3564b684d727b2b12e4b1b4e14bd73a0d4345
sergsenta1995 <sergsenta1995@gmail.com>
2016-05-30 20:37:57 +0400

    add test main_test.go

--- main_test.go
+++ main_test.go
@@ -4,9 +4,26 @@ import (
 	"testing"
 )
 
+type testpair struct {
+	value int
+	numeric int
+}
+
+var tests = []testpair {
+	{1, "один"},
+	{10, "десять"},
+	{100, "сто"},
+	{1000, "тысяча"}}
+
 func TestIntToNumeric(t *testing.T) {
-	if IntToNumeric(1) != "один" {
-		t.Error("Число состоит из одного разряда.")
-		t.Error("Ошибка в определении первого разряда")
+	for _, pair := range tests {
+		result := IntToNumeric(pair.value)
+		if result != pair.numeric {
+			t.Error(
+				"Для значения ", pair.value,
+				"допустиый результат ", pair.numeric,";",
+				"Получено ", result,
+			)
+		}
 	}
 }
