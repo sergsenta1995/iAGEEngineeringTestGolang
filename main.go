36ebd3088b1105eba7bb2664295f221eea47b47d
sergsenta1995 <sergsenta1995@gmail.com>
2016-05-31 09:52:02 +0400

    update functions main.go

--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 	"strconv"
 	"errors"
+	"bufio"
+	"os"
 )
 
-var ones = [][]string {
-	{"один ",   "одна "},
-	{"два ",    "две "},
-	{"три ",    "три "},
-	{"четыре ", "четыре "},
-	{"пять ",   "пять "},
-	{"шесть ",  "шесть "},
-	{"семь ",   "семь "},
-	{"восемь ", "весемь "},
-	{"девять ", "девять"}}
+var ones = []string {
+	"один ",
+	"два ",
+	"три ",
+	"четыре ",
+	"пять ",
+	"шесть ",
+	"семь ",
+	"восемь ",
+	"девять "}
 
 var triads = [][]string{
 	// падежи
@@ -39,10 +41,10 @@ var tens = []string{
 	"двадцать ",
 	"тридцать ",
 	"сорок ",
-	"пятдесят ",
-	"шестдесят ",
-	"семдесят ",
-	"восемдесят ",
+	"пятьдесят ",
+	"шестьдесят ",
+	"семьдесят ",
+	"восемьдесят ",
 	"девяносто "}
 
 var thousands = []string{
@@ -50,41 +52,57 @@ var thousands = []string{
 	"двести ",
 	"триста ",
 	"четыреста ",
-	"пятсот ",
+	"пятьсот ",
 	"шестьсот ",
-	"семсот ",
-	"восемсот ",
-	"девятсот "}
+	"семьсот ",
+	"восемьсот ",
+	"девятьсот "}
+
+// Особы индекс для тысячи, т. к. тысячи, заканчивающиеся на '1' имеют женски род
+const thousand = 1
 
 func IntToNumeric(number int) string {
 	var triadNumeric string
 	var result string
+
+	if number == 0 {
+		return "ноль"
+	}
+
 	for triadNumber := 0; number >= 1; triadNumber++ {
 		triad := number % 1000
 		number /= 1000
+
 		if triad >= 100 {
 			triadNumeric += thousands[triad/100-1]
 			triad %= 100
 		}
+
 		if triad >= 20 || triad == 10 {
 			triadNumeric += tens[triad/10-1]
 			triad %= 10
 		}
+
 		if triad >= 11 {
 			triadNumeric += onesTens[triad/10-1]
 		}
+
 		if triad == 1 {
-			triadNumeric += ones[triad-1][triadNumber]
-			triadNumeric += triads[triadNumber][0]
+			// Только тысяча, заканчивающаяся на '1' может иметь женский род
+			if triadNumber == thousand {
+				triadNumeric += "одна "
+				triadNumeric += triads[triadNumber][0]
+			} else {
+				triadNumeric += ones[triad-1]
+				triadNumeric += triads[triadNumber][0]
+			}
 		} else {
 			if triad >= 2 && triad <= 4 {
-				triadNumeric += ones[triad-1][triadNumber]
+				triadNumeric += ones[triad-1]
 				triadNumeric += triads[triadNumber][1]
 			} else {
 				if triad >= 5 && triad <= 9 {
-					triadNumeric += ones[triad-1][triadNumber]
-					triadNumeric += triads[triadNumber][2]
-				} else {
+					triadNumeric += ones[triad-1]
 					triadNumeric += triads[triadNumber][2]
 				}
 			}
@@ -92,6 +110,7 @@ func IntToNumeric(number int) string {
 		result = triadNumeric + result
 		triadNumeric = ""
 	}
+	// Убрать лишний пробельный символ
 	return result[0:len(result)-1]
 }
 
@@ -110,7 +129,17 @@ func CheckNumber(stringNumber string) (intNumber int, err error) {
 }
 
 func main ()  {
-
-
-	fmt.Println(IntToNumeric(1000))
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	fmt.Println("Введите число от 0 до 1000000: ")
+	stringNumber, _:= reader.ReadString('\n')
+	// Убрать символ конца строки - без него не преобразуется в число
+	stringNumber = stringNumber[0:len(stringNumber)-1]
+	
+	intNumber, err := CheckNumber(stringNumber)
+	if err == nil {
+		numeric := IntToNumeric(intNumber)
+		fmt.Println(numeric)
+	} else {
+		fmt.Println(err)
+	}
 }
